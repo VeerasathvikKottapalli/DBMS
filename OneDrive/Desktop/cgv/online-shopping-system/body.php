f18c068fac45f065383471d03d405df5847c571b
veera satvik <veerasatvik@gmail.com>
2022-06-02 14:12:05 +0530

    first commit

---
+++ OneDrive/Desktop/cgv/online-shopping-system/body.php
@@ -0,0 +1,99 @@
+
+   <div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+      <center>  <img src="img/cricket.jpg" alt="Los Angeles" style="width:97%;"></center>
+        
+      </div>
+
+      <div class="item">
+        <img src="img/badminton sport.jpg" style="width:100%;">
+        
+      </div>
+    
+      <div class="item">
+        <img src="img/football.jpg" alt="New York" style="width:100%;">
+        
+      </div>
+      </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control _26sdfg" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only" >Previous</span>
+    </a>
+    <a class="right carousel-control _26sdfg" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+     
+
+
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container 
+			<div class="container">
+			
+				<!-- row 
+				<div class="row">
+					<!-- shop 
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=78"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop01.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Laptop<br>Collection</h3>
+								<a href="product.php?p=78" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop 
+
+					<!-- shop 
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=72"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop03.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Accessories<br>Collection</h3>
+								<a href="product.php?p=72" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop 
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=79"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/shop02.png" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>Cameras<br>Collection</h3>
+								<a href="product.php?p=79" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+                            </div></a>
+					</div>
+					<!-- /shop 
+				</div>
+				<!-- /row 
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+   </div>
+   
